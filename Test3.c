0fb6b2d232d47ea6dab445a56f6cdb36fad5a0c5
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2022-03-08 17:05:27 -0600

    Add files via upload

---
+++ Test3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int num = 6;
+    printf("%d", num);
+    printf("\n%f", 5.0 + 4.5);
+    printf("\n%f", 5.0 - 4.5);
+    printf("\n%f", 5 / 4.0);
+    printf("\n%f", pow(2, 3) ); //This is a comment /*This too*/
+    printf("\n%f", sqrt(36) );
+    printf("\n%f", ceil(35.365) );
+    printf("\n%f", floor(35.365) );
+    return 0;
+}
